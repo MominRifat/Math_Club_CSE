c3cff3143d96efe983d36a794787d86b87ad72e4
Momin_Rifat <mominrifat8@gmail.com>
2026-05-01 02:51:50 +0600

    responsive updates

--- quiz_app/models.py
+++ quiz_app/models.py
@@ -24,28 +24,36 @@ class Quiz(models.Model):
         else:
             return "FINISHED"
 
+
 class Question(models.Model):
     quiz = models.ForeignKey(Quiz, related_name='questions', on_delete=models.CASCADE)
     text = models.TextField()
     image = models.ImageField(upload_to='quiz_questions/', blank=True, null=True)
     
-    # Text Options
     opt1 = models.CharField(max_length=255)
     opt2 = models.CharField(max_length=255)
     opt3 = models.CharField(max_length=255)
     opt4 = models.CharField(max_length=255)
     
-    # Image Options (Optional)
     opt1_img = models.ImageField(upload_to='option_images/', blank=True, null=True)
     opt2_img = models.ImageField(upload_to='option_images/', blank=True, null=True)
     opt3_img = models.ImageField(upload_to='option_images/', blank=True, null=True)
     opt4_img = models.ImageField(upload_to='option_images/', blank=True, null=True)
     
-    correct_option = models.IntegerField(choices=[(1, 'Option 1'), (2, 'Option 2'), (3, 'Option 3'), (4, 'Option 4')])
+    # 🔥 CHANGE HERE (important)
+    correct_option = models.IntegerField(
+        choices=[(1, 'Option 1'), (2, 'Option 2'), (3, 'Option 3'), (4, 'Option 4')],
+        null=True,
+        blank=True
+    )
+
     marks = models.IntegerField(default=1)
+
+    is_required = models.BooleanField(default=False)
 
     def __str__(self):
         return self.text[:50]
+
 
 class Participant(models.Model):
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
