48d417a38d746a0730cc68331b2dc5a31ebf2d89
Momin_Rifat <mominrifat8@gmail.com>
2026-05-01 02:57:02 +0600

    add

--- math_club_project/settings.py
+++ math_club_project/settings.py
@@ -79,7 +79,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 TIME_ZONE = 'Asia/Dhaka'
